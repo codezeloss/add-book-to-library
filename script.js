2f7ea2d464c62b05484722d082c94b9e06dbed85
Elhoussaine AIT AICHTE <elhoussaineaitX@gmail.com>
2022-04-23 14:49:32 +0000

    commit

--- script.js
+++ script.js
@@ -3,53 +3,76 @@
 const bookTitle = document.getElementById("title");
 const bookAuthor = document.getElementById("author");
 const bookPages = document.getElementById("pages");
-const readStatus = document.getElementById("read");
+const readStatusSelected = document.getElementById("read-status-selected");
 const submitBtn = document.getElementById("submit");
-const deleteBtn = document.getElementsByClassName("delete-btn");
+const deleteCardBtn = document.getElementsByClassName("delete-btn");
 const displayBooks = document.querySelector(".books-cards-section");
 
 let myLibrary = [];
-
-function Book() {
-  // the constructor...
-  this.title = bookTitle.value;
-  this.author = bookAuthor.value;
-  this.pages = bookPages.value;
-  this.read = readStatus.value;
-}
-
-function addBookToLibrary() {
-  // do stuff here
-  let book = new Book();
-  myLibrary.push(book);
-  displayBooksInfo();
-}
-
-function displayBooksInfo() {
-  // do stuff here
-  let booksInfos = "";
-  for (let i = 0; i < myLibrary.length; i++) {
-    booksInfos += `
-    <div class="book-card">
-      <div class="book-card-header">
-        <h2 class="heading-secondary book-card-title">${myLibrary[i].title}</h2>
-        <h3 class="heading-teritary book-card-author">${myLibrary[i].author}</h3>
-      </div>
-      <img src="img/books-img.jpg" alt="" />
-      <div class="book-card-status">
-        <p class="pages-number">Pages : <b>${myLibrary[i].pages}</b></p>
-        <p class="read-status-el">Read Status : <b>${myLibrary[i].read}</b></p>
-      </div>
-      <button class="delete-btn" type="button">Delete</button>
-    </div>`;
+class Library {
+  constructor() {
+    this.title = bookTitle.value;
+    this.author = bookAuthor.value;
+    this.pages = bookPages.value;
+    this.read = readStatusSelected.value;
   }
 
-  if (bookTitle.value && bookAuthor.value && bookPages.value) {
-    submitBtn.disabled = false;
-    displayBooks.innerHTML = booksInfos;
-  } else {
-    submitBtn.disabled = true;
+  addBookToLibrary() {
+    myLibrary.push(this);
+  }
+
+  displayBooksInfo() {
+    // do stuff here
+    let booksInfos = "";
+    for (let i = 0; i < myLibrary.length; i++) {
+      booksInfos += `
+      <div class="book-card">
+        <div class="book-card-header">
+          <h2 class="heading-secondary book-card-title">${myLibrary[i].title}</h2>
+          <h3 class="heading-tertiary book-card-author">${myLibrary[i].author}</h3>
+        </div>
+        <img src="img/books-img.jpg" alt="" />
+        <div class="book-card-status">
+          <p class="pages-number">Pages : <b>${myLibrary[i].pages}</b></p>
+          <p class="read-status-el">Read Status : <b>${myLibrary[i].read}</b></p>
+        </div>
+        <button id="delete" class="delete-btn" type="button">Delete</button>
+      </div>`;
+
+      displayBooks.innerHTML = booksInfos;
+    }
+
+    // if (bookTitle.value && bookAuthor.value && bookPages.value) {
+    //   submitBtn.disabled = false;
+    //   displayBooks.innerHTML = booksInfos;
+    // } else {
+    //   submitBtn.disabled = true;
+    // }
+
+    // delete book
+    for (let i = 0; i < deleteCardBtn.length; i++) {
+      deleteCardBtn[i].addEventListener("click", function () {
+        displayBooks.removeChild(this.parentElement);
+      });
+    }
+
+    // clear inputs
+    bookTitle.value = "";
+    bookAuthor.value = "";
+    bookPages.value = "";
+    readStatusSelected.value = "";
+
+    // Submit book to library and display it  in the library section
+    submitBtn.addEventListener("click", function () {
+      let newBook = new Library();
+      newBook.addBookToLibrary();
+      newBook.displayBooksInfo();
+    });
   }
 }
 
-submitBtn.addEventListener("click", addBookToLibrary);
+// Run the library class and display books info on the page when the page is loaded or refreshed (onload) or when the user clicks on the submit button (onclick)
+window.onload = function () {
+  let newBook = new Library();
+  newBook.displayBooksInfo();
+};
